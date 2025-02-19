5ae0d30d9d5d4be0a62d58feb59bf85de7ef87f1
futurelithics <chad.denaux@gmail.com>
2025-02-19 17:15:21 -0500

    migration issues

---
+++ config/initializers/custom_logging.rb
@@ -0,0 +1,26 @@
+require 'active_support/logger'
+
+# Monkey patch the formatter to add colors
+module ActiveSupport
+  class Logger
+    class SimpleFormatter
+      SEVERITY_TO_COLOR_MAP = {
+        'DEBUG'   => "\033[0;37m", # White
+        'INFO'    => "\033[0;36m", # Cyan
+        'WARN'    => "\033[0;33m", # Yellow
+        'ERROR'   => "\033[0;31m", # Red
+        'FATAL'   => "\033[0;31m", # Red
+        'UNKNOWN' => "\033[0;32m"  # Green
+      }.freeze
+
+      RESET_COLOR = "\033[0m"
+
+      def call(severity, timestamp, progname, msg)
+        color = SEVERITY_TO_COLOR_MAP[severity] || SEVERITY_TO_COLOR_MAP['UNKNOWN']
+        formatted_severity = sprintf("%-5s", severity)
+        formatted_time = timestamp.strftime("%Y-%m-%d %H:%M:%S.%L")
+        "#{color}[#{formatted_severity} #{formatted_time}] #{msg}#{RESET_COLOR}\n"
+      end
+    end
+  end
+end 
